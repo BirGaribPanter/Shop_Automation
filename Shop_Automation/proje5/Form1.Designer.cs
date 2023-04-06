c4c0d41575b6ca8802916e14e11afb4cc84afcfa
Cemal Matır <116919080+BirGaribPanter@users.noreply.github.com>
2023-04-06 20:21:46 +0300

    Add files via upload

---
+++ Shop_Automation/proje5/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace proje5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.DGVall = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVall)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(113, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(191, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(113, 92);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(191, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(113, 138);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(191, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // DGVall
+            // 
+            this.DGVall.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVall.Location = new System.Drawing.Point(644, 12);
+            this.DGVall.Name = "DGVall";
+            this.DGVall.Size = new System.Drawing.Size(427, 541);
+            this.DGVall.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(113, 283);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "ÜRÜN ADI";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "FİYAT";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "ADET";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "TARİH";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(113, 182);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 12;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(255, 283);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "satış";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1190, 596);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DGVall);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGVall)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DataGridView DGVall;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Shop_Automation/proje5/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace proje5
+{
+    public partial class Form1 : Form
+    {
+        string tarih;
+        IDataReader dr;
+        OleDbConnection con;
+        OleDbDataAdapter da;
+        OleDbCommand cmd;
+        DataSet ds;
+        void vericek()//Veritabanından verileri çekme işlem--i
+        {
+
+            con = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Database8.accdb");
+            da = new OleDbDataAdapter("Select urunler,fiyat,adet,tarih from halprogramı", con);
+            ds = new DataSet();
+            con.Open();
+            da.Fill(ds, "halprogramı");
+            DGVall.DataSource = ds.Tables["halprogramı"];
+            con.Close();
+            DGVall.ColumnHeadersDefaultCellStyle.BackColor = Color.Aqua;
+
+            DGVall.Columns[0].HeaderText = "urunler";
+            DGVall.Columns[1].HeaderText = "fiyat";
+            DGVall.Columns[2].HeaderText = "adet";
+            DGVall.Columns[3].HeaderText = "tarih";
+            
+        }
+
+        void temizle()
+        {
+            dateTimePicker1.Value = Convert.ToDateTime(tarih);
+            
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+           
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" )
+            {
+                temizle();
+                MessageBox.Show("ÜRÜN BİLGİSİ GİR!", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (textBox2.Text == "")
+            {
+                temizle();
+                MessageBox.Show("ÜRÜN BİLGİSİ GİRİN!", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (textBox3.Text == "")
+            {
+                temizle();
+                MessageBox.Show("ÜRÜN BİLGİSİ GİRİN!", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            
+            else
+            {
+                cmd = new OleDbCommand();
+                cmd.Connection = con;
+                con.Open();
+                cmd.CommandText = "insert into halprogramı (urunler,fiyat,adet,tarih) values ('" + textBox1.Text + "','" +int.Parse (textBox2.Text) + "','" + int.Parse(textBox3.Text) + "','" + dateTimePicker1.Value + "')";
+                cmd.ExecuteNonQuery();
+                con.Close();
+                temizle();
+                vericek();
+                MessageBox.Show("ÜRÜN EKLEME İŞLEMİ BAŞARILI!", "Ekleme İŞlemi Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tarih = dateTimePicker1.Value.ToString();
+            vericek();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" )
+            {
+                temizle();
+                MessageBox.Show("ÜRÜN BİLGİSİ GİR!!", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (textBox2.Text == "")
+            {
+                temizle();
+                MessageBox.Show("ÜRÜN BİLGİSİ GİR!!", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (textBox3.Text == "")
+            {
+                temizle();
+                MessageBox.Show("ÜRÜN BİLGİSİ GİR!!", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            else
+            {
+
+
+
+
+
+
+                 
+                 cmd = new OleDbCommand();
+                 con.Open();
+                 cmd.Connection = con;
+                 cmd.CommandText = "update halprogramı set fiyat='" + textBox2.Text + "',adet='" + textBox3.Text + "',tarih='" + dateTimePicker1.Text + "' where urunler='" + textBox1.Text + "'";
+                 cmd.ExecuteNonQuery();
+                 con.Close();
+                 vericek();
+                 temizle();
+                 MessageBox.Show("HAL Güncelleme İşlemi Başarılı!", "Güncelleme İŞlemi Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+               
+            }
+        }
+    }
+}
+//Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\PC\Desktop\projeler\proje5\proje5\bin\Debug\Database8.accdb
